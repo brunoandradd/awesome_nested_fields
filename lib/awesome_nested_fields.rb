a71e88fcc4793d1760655723181cbb06e1da7d6c
Bruno Andrade <bruno77sa@gmail.com>
2011-07-05 16:13:00 -0300

    1 removed bugs and add support a ruby 1.8

--- lib/awesome_nested_fields.rb
+++ lib/awesome_nested_fields.rb
@@ -4,6 +4,12 @@ module AwesomeNestedFields
   else
     require 'awesome_nested_fields/engine'
   end
-  
   require 'awesome_nested_fields/version'
+  
+  module_path =  File.expand_path('../../', __FILE__)+'/app/helpers/'
+  if RUBY_VERSION.include?('1.8')
+    require(module_path+'awesome_nested_fields_helper18')  
+  else
+    require(module_path+'awesome_nested_fields_helper')  
+  end
 end
