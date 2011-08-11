82ddfbd581ec9517a07225d109e2a63efe27b7df
Bruno Andrade <bruno77sa@gmail.com>
2011-08-11 13:47:06 -0300

    resolved bug fix in ruby 1.8

--- lib/awesome_nested_fields/railtie.rb
+++ lib/awesome_nested_fields/railtie.rb
@@ -5,13 +5,16 @@ module AwesomeNestedFields
     end
 
     initializer "AwesomeNestedFields.init" do |app|
-     module_path =  File.expand_path('../../..', __FILE__)+ '/app/helpers/'
-     require(module_path+'awesome_nested_fields_helper')  
-
+      module_path =  File.expand_path('../../../', __FILE__)+'/app/helpers/'
+      if RUBY_VERSION.include?('1.8')
+        require(module_path+'awesome_nested_fields_helper18')
+      else
+        require(module_path+'awesome_nested_fields_helper')
+      end
       ActionView::Base.class_eval do
         include AwesomeNestedFieldsHelper
       end
     end
-    
+
   end
 end
